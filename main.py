11912fce9303ed825473a28337906959ddac143a
darieloespinal <doe2102@columbia.edu>
2023-11-25 06:18:20 +0000

    Implementing Operations

--- main.py
+++ main.py
@@ -1,12 +1,82 @@
-import flask
+from fastapi import FastAPI, HTTPException
 
-app = flask.Flask(__name__)
+app = FastAPI()
 
+# Your team management logic and database configuration go here
 @app.get("/")
-def hello():
+async def root():
 
     return "This microservice will be used for the team managment functionality.\n"
 
+"""
+GET operation to retrieve team information by ID
+"""
+
+@app.get("/teams/{team_id}")
+
+async def get_team(team_id: str):
+    try:
+        # Implement logic to retrieve team information from the database
+
+        # To be modified with actual database query
+        team_info = {"team_id": team_id, "name": "Sample Team", "coach": "John Doe"}
+
+        if team_info:
+            return team_info
+        else:
+            raise HTTPException(status_code=404, detail="Team not found")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
+
+"""
+POST operation to add a new team
+"""
+
+@app.post("/teams")
+async def add_team(team_data: dict):
+    try:
+        # Implement logic to add a new team to the database
+
+        # To be modified with actual database insertion
+        team_id = "new_team_id"
+        return {"message": f"Team {team_id} added successfully"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
+
+"""
+PUT operation to modify team information by ID
+"""
+
+@app.put("/teams/{team_id}")
+async def modify_team(team_id: str, team_data: dict):
+    try:
+        # Implement logic to modify team information in the database
+
+        # To be modified with actual database update
+        return {"message": f"Team {team_id} modified successfully"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
+
+"""
+DELETE operation to delete a team by ID
+"""
+
+@app.delete("/teams/{team_id}")
+async def delete_team(team_id: str):
+    try:
+        # Implement logic to delete a team from the database
+
+        # To be modified with actual database deletion
+        return {"message": f"Team {team_id} deleted successfully"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
+
 if __name__ == "__main__":
-    
-    app.run(host="localhost", port=8080, debug=True)
+    # Run the FastAPI application
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
+
