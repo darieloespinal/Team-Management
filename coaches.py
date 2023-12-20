2e5381d7c24c29dba5c4af60a6e0be6e5841b82b
darieloespinal <doe2102@columbia.edu>
2023-12-20 05:33:27 +0000

    Created coaches.py and modified main.py

---
+++ coaches.py
@@ -0,0 +1,90 @@
+from pydantic import BaseModel
+import sqlalchemy as db_al
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Define Pydantic data model for a coach
+class CoachModel(BaseModel):
+    id: int
+    name: str
+    team: str = None
+    birthdate: str = None
+    startdate: int = None
+    enddate: str = None
+    email: str = None
+    wins: int = None
+    losses: int = None
+
+# Class to interact with the database for coach-related operations
+class CoachResource:
+    def __init__(self):
+        db_username = os.getenv("DB_USERNAME")
+        db_password = os.getenv("DB_PASSWORD")
+        db_host = os.getenv("DB_HOST")
+        db_port = str(os.getenv("DB_PORT"))
+        db_name = os.getenv("DB_NAME")
+
+        # Create a SQLAlchemy engine
+        db_url = f"mysql+mysqlconnector://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
+        self.engine = db_al.create_engine(db_url)
+        self.conn = self.engine.connect()
+
+        # Metadata instance for extracting the metadata: structures, table infos, etc.
+        self.metadata = db_al.MetaData()
+
+        # Table object for the coaches table
+        self.coaches_table = db_al.Table('Head_Coaches', self.metadata, autoload_with=self.engine)
+
+    def get_coach_by_id(self, coach_id):
+        query = self.coaches_table.select().where(self.coaches_table.columns.id == coach_id)
+        exe = self.conn.execute(query)
+        #result = exe.fetchone()
+        #return result
+
+        column_names = exe.keys()
+        result = exe.fetchall()
+        result_dicts = [dict(zip(column_names, row)) for row in result]
+
+        return result_dicts
+
+
+    def add_coach(self, coach: CoachModel):
+        ins_query = self.coaches_table.insert().values(
+            id=coach.id,
+            name=coach.name,
+            team=coach.team,
+            birthdate=coach.birthdate,
+            startdate=coach.startdate,
+            enddate=coach.enddate,
+            email=coach.email,
+            wins=coach.wins,
+            losses=coach.losses
+        )
+        result = self.conn.execute(ins_query)
+        self.conn.commit()
+        return result
+
+    def modify_coach(self, coach: CoachModel, coach_id: int):
+        update_data = {
+            "name": coach.name,
+            "team": coach.team,
+            "birthdate": coach.birthdate,
+            "startdate": coach.startdate,
+            "enddate": coach.enddate,
+            "email": coach.email,
+            "wins": coach.wins,
+            "losses": coach.losses
+        }
+        update_query = self.coaches_table.update().where(self.coaches_table.columns.id == coach_id).values(update_data)
+        result = self.conn.execute(update_query)
+        self.conn.commit()
+        return result
+
+    def delete_coach(self, coach_id):
+        del_query = self.coaches_table.delete().where(self.coaches_table.columns.id == coach_id)
+        result = self.conn.execute(del_query)
+        self.conn.commit()
+        return result
--- main.py
+++ main.py
@@ -1,212 +1,130 @@
-from fastapi import FastAPI, HTTPException
+# Import libraries here
+from fastapi import FastAPI, HTTPException, Response, Query, Request
 from fastapi.responses import HTMLResponse
 import uvicorn
 import mysql.connector
+from coaches import CoachModel, CoachResource  # Importing CoachModel and CoachResource from coaches.py
 
 app = FastAPI()
+coach_resource = CoachResource()
 
 # Updated database and table names
 db_config = {
-#    "host": "team-mgmt-402907:us-east1:sql-team-mgmt",
     "host": "34.23.76.102",
     "database": "Managers",
     "user": "doe2102",
     "password": "sql_for_mgmt",
 }
 
+"""
+GET operations here
+"""
 
-def get_team_from_db(id):
-    connection = mysql.connector.connect(**db_config)
-    cursor = connection.cursor(dictionary=True)
-    id = 1
-    query = f"SELECT * FROM Head_Coaches WHERE id = '{id}'"
-    cursor.execute(query)
-    team_info = cursor.fetchone()
-    cursor.close()
-    connection.close()
-    return team_info
-
-def add_team_to_db(team_data):
-    connection = mysql.connector.connect(**db_config)
-    cursor = connection.cursor()
-    query = f"INSERT INTO Head_Coaches (name, team, birthdate, startdate, enddate, email, wins, losses) " \
-            f"VALUES ('{team_data['name']}', '{team_data['team']}', '{team_data['birthdate']}', " \
-            f"'{team_data['startdate']}', '{team_data['enddate']}', '{team_data['email']}', " \
-            f"{team_data['wins']}, {team_data['losses']})"
-    cursor.execute(query)
-    connection.commit()
-    cursor.close()
-    connection.close()
-    return {"message": "Team added successfully"}
-
-def modify_team_in_db(id, team_data):
-    connection = mysql.connector.connect(**db_config)
-    cursor = connection.cursor()
-    query = f"UPDATE Head_Coaches SET name = '{team_data['name']}', team = '{team_data['team']}', " \
-            f"birthdate = '{team_data['birthdate']}', startdate = '{team_data['startdate']}', " \
-            f"enddate = '{team_data['enddate']}', email = '{team_data['email']}', " \
-            f"wins = {team_data['wins']}, losses = {team_data['losses']}' WHERE id = '{id}'"
-    cursor.execute(query)
-    connection.commit()
-    cursor.close()
-    connection.close()
-    return {"message": "Team modified successfully"}
-
-def delete_team_from_db(id):
-    id = 8
-    connection = mysql.connector.connect(**db_config)
-    cursor = connection.cursor()
-    query = f"DELETE FROM Head_Coaches WHERE id = '{id}'"
-    cursor.execute(query)
-    connection.commit()
-    cursor.close()
-    connection.close()
-    return {"message": "Team deleted successfully"}
+@app.get("/")
+async def root():
+    return {"message": "From Your Name, microservice - Coach searching, work in progress"}
 
 """
-GET operation to retrieve team information by ID
+Show the coach information based on the coach_id as an HTML table webpage
 """
-@app.get("/teams/{id}")
-async def get_team(id: str):
-   try:
-       team_info = get_team_from_db(id)
-       if team_info:
-           return team_info
-       else:
-           raise HTTPException(status_code=404, detail="Team not found")
+@app.get("/v1/coaches/{coach_id}/info", response_class=HTMLResponse)
+async def coach_info_by_id(coach_id: int):
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
 
+        query = f"SELECT * FROM coach_info WHERE coach_id = {coach_id}"
 
-   except Exception as e:
-       raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
+        cursor.execute(query)
+        rows = cursor.fetchall()
 
+        cursor.close()
+        connection.close()
 
-"""
-POST operation to add a new team
-"""
-@app.post("/teams")
-async def add_team(team_data: dict):
-   try:
-       result = add_team_to_db(team_data)
-       return result
+        if len(rows) == 0:
+            message = f"Sorry, no data found for coach ID: {coach_id}, please check the input."
+            return Response(content=message, media_type="text/plain", status_code=200)
+        else:
+            message = "<html><body>"
+            message += "Here is the coach information you requested:"
+            message += "<table border='1'>"
+            message += "<tr>"
+            for column_name in cursor.column_names:
+                message += f"<th>{column_name}</th>"
+            message += "</tr>"
+            for row in rows:
+                message += "<tr>"
+                for value in row:
+                    message += f"<td>{value}</td>"
+                message += "</tr>"
+            message += "</table></body></html>"
 
+        return HTMLResponse(content=message, status_code=200)
 
-   except Exception as e:
-       raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
-
-
-"""
-PUT operation to modify team information by ID
-"""
-@app.put("/teams/{id}")
-async def modify_team(id: str, team_data: dict):
-   try:
-       result = modify_team_in_db(id, team_data)
-       return result
-
-
-   except Exception as e:
-       raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
-
+    except Exception as e:
+        return Response(content=f"Error: {str(e)}", media_type="text/plain", status_code=500)
 
 """
-DELETE operation to delete a team by ID
+POST operation to add a new coach
 """
-@app.delete("/teams/{id}")
-async def delete_team(id: str):
-   try:
-       result = delete_team_from_db(id)
-       return result
+@app.post("/coaches")
+async def add_coach(coach_data: CoachModel):
+    try:
+        result = coach_resource.add_coach(coach_data)
+        coach_id = coach_data.coach_id
+        coach_name = coach_data.name
+        
+        message_content = f'New Coach {coach_name} Added to the database!'
+        subject="Coach Added"
+        
+        GET_response = await player_by_id(player_id)
+           
+        return GET_response
+
+    except Exception as e:
+        return Response(content=f"Error: {str(e)}", media_type="text/plain", status_code=500)
+
+"""
+PUT operation to modify coach information by ID
+"""
+@app.put("/coaches/{coach_id}")
+async def modify_coach(coach_id: int, coach_data: CoachModel):
+    try:
+        coach_resource.modify_coach(coach_id, coach_data)
+        
+        GET_response = await player_by_id(player_id)
+        return GET_response
 
 
-   except Exception as e:
-       raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
+    except Exception as e:
+        #raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
+        return Response(content=f"Error: {str(e)}", media_type="text/plain", status_code=500)
 
+"""
+DELETE operation to delete a coach by ID
+"""
+@app.delete("/coaches/{coach_id}")
+async def delete_coach(coach_id: int):
+    try:
+        coach_resource.delete_coach(coach_id)
+
+        message = "Complete."
+        return Response(content=message, media_type="text/plain", status_code=200)
+
+
+    except Exception as e:
+        #raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
+        return Response(content=f"Error: {str(e)}", media_type="text/plain", status_code=500)
 
 if __name__ == "__main__":
-   #uvicorn.run(app, host="35.185.107.184", port=8000)
+    try:
+        connection = mysql.connector.connect(**db_config)
+        print("Connected to MySQL")
+    except Exception as e:
+        print(f"Error: {str(e)}")
 
-   try:
-    connection = mysql.connector.connect(**db_config)
-    print("Connected to MySQL")
-   except Exception as e:
-    print(f"Error: {str(e)}")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
-   
-   connection = mysql.connector.connect(**db_config)
-   cursor = connection.cursor()
-   id = 1
-   query = f"UPDATE Head_Coaches SET name = 'Bobby' WHERE id = '{id}'"
-   cursor.execute(query)
-   connection.commit()
-   team_info = cursor.fetchall()
-   cursor.close()
-   connection.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
-"""
-   connection = mysql.connector.connect(**db_config)
-   cursor = connection.cursor()
-   query = f"INSERT INTO Head_Coaches (id, name, team, birthdate, startdate, enddate, email, wins, losses) " \
-        f"VALUES (8, 'wayne bennett', 'the dolphins', '1950-01-01', 1987, 'present', 'wb1987@nfl.com', 562, 323)"
-   cursor.execute(query)
-   connection.commit()
-
-   id = 8
-   query = f"SELECT * FROM Head_Coaches WHERE id = '{id}'"
-   cursor.execute(query)
-   team_info = cursor.fetchall()
-   
-   print(team_info)
-
-   cursor.close()
-   connection.close()
-   
- """  
-
-   
-   
-
-"""
-id = 8
-cursor = connection.cursor()
-query = f"DELETE FROM Head_Coaches WHERE id = '{id}'"
-cursor.execute(query)
-query = f"SELECT COUNT(id) FROM Head_Coaches"
-cursor.execute(query)
-team_info = cursor.fetchall()
-print(team_info)
-
-"""
-
-
-
-
-
-"""
-connection = mysql.connector.connect(**db_config)
-cursor = connection.cursor()
-id = 1
-query = f"SELECT * FROM Head_Coaches WHERE id = '{id}'"
-cursor.execute(query)
-team_info = cursor.fetchone()
-cursor.close()
-connection.close()
-print(team_info)
-"""
-
-uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
 
